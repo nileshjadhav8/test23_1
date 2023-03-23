f75020dff9d0884f5f371b864ae4c120f572f6b9
Nilesh <nil.y.jadhav@gmail.com>
2023-03-23 15:53:08 +0800

    initial commit

---
+++ src/main/java/com/example/test_23_1/Test231Application.java
@@ -0,0 +1,13 @@
+package com.example.test_23_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test231Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_23_1/Test231ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_23_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
